5e95d5442d212ab04edc10aa9962a97953e16021
danlee01 <59035722+danlee01@users.noreply.github.com>
2020-06-28 15:36:08 -0700

    complete

--- sketch.js
+++ sketch.js
@@ -1,25 +1,25 @@
+let walls = [];
+let particle;
 
-
-let wall;
-let ray;
 function setup() {
-  createCanvas(400, 400);
-  wall = new Boundary(300, 100, 300, 300);
-  ray = new Ray(100, 200);
+  createCanvas(600, 400);
+  for (let i = 0; i < 5; i++) {
+    let x1 = random(width);
+    let x2 = random(width);
+    let y1 = random(height);
+    let y2 = random(height);
+    walls[i] = new Boundary(x1, y1, x2, y2);
+  }
+
+  particle = new Particle();
 }
 
 function draw() {
   background(0);
-  wall.show();
-  ray.show();
-  ray.lookAt(mouseX, mouseY);
-
-  let pt = ray.cast(wall);
-
-  if (pt) {
-    console.log( pt.x + ", " + pt.y);
-    console.log(mouseX, mouseY);
-    fill(255);
-    ellipse(pt.x, pt.y, 8, 8);
+  for (let wall of walls) {
+    wall.show();
   }
+  particle.update(mouseX, mouseY);
+  particle.show();
+  particle.look(walls);
 }
